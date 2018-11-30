f137357c3baef40e03d129b8433bf433a9bc1e88
Lynn Miro <lynnmiro65@gmail.com>
2018-11-29 20:46:12 -0600

    Testing connection to DB and create a sample table

--- final/src/models/DaoModel.java
+++ final/src/models/DaoModel.java
@@ -16,8 +16,9 @@ public class DaoModel {
 	 conn = new DBConnect();
 	}
 	// CREATE TABLE METHOD
+	// CREATE TABLE METHOD
 	public void createTable() {
-	
+	 try {
 
 	 // Open a connection
 	 System.out.println("Connecting to database to create Table...");
@@ -26,29 +27,21 @@ public class DaoModel {
 	 // Execute create query
 	 System.out.println("Creating table in given database...");
 
-	 try {
-		stmt = conn.connect().createStatement();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+	 stmt = conn.connect().createStatement();
 
-		 String sql = "CREATE TABLE fp510LNT ";  
-		        
-	 try {
-		stmt.executeUpdate(sql);
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+		 String sql = "CREATE TABLE fplnt " + 
+		              "(pid INTEGER not NULL AUTO_INCREMENT, " + 
+		  	        " id VARCHAR(10), " +
+				  " income numeric(8,2), " + 
+				  " pep VARCHAR(3), " + 
+			  " PRIMARY KEY ( pid ))";
+	 stmt.executeUpdate(sql);
 	 System.out.println("Created table in given database...");
-	 try {
-		conn.connect().close();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} 
-	
+	 conn.connect().close(); //close db connection 
+	}catch (SQLException se) { // Handle errors for JDBC
+	 se.printStackTrace();
 	}
+	}
+	
 }
 //close db connection 
