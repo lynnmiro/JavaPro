ce0101fb9e63d7489dccc0ac80e5bb03b0dd7ab2
Lynn Miro <lynnmiro65@gmail.com>
2018-11-30 15:53:25 -0600

    Main App is standing and running the loginview

--- final/src/application/Main.java
+++ final/src/application/Main.java
@@ -1,31 +1,40 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import models.DaoModel;
+//import models.DaoModel;
 
 
 public class Main extends Application {
+public static Stage stage;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+			stage = primaryStage;
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass()
+					.getResource("/views/LoginView.fxml"));
+			Scene scene = new Scene(root);
+			stage.setTitle("Login");
+			stage.setScene(scene);
+			stage.show();
+
+		} catch (Exception e) {
+			System.out.println("Error occured while inflating view: " + e);
 		}
 	}
+
+
+
 	
 	public static void main(String[] args) {
 		
-		//launch(args);
+		launch(args);
 		
-		DaoModel dao = new DaoModel();
+		//DaoModel dao = new DaoModel();
 		//dao.createTable();
 	}
 }
