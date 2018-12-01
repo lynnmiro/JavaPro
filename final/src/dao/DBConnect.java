e67c621064b26bad7cb76c14fd62f38f352f4b21
Lynn Miro <lynnmiro65@gmail.com>
2018-11-30 22:08:59 -0600

    Login is working now for Admin and User

--- final/src/dao/DBConnect.java
+++ final/src/dao/DBConnect.java
@@ -2,7 +2,6 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DBConnect  {
@@ -12,22 +11,14 @@ public class DBConnect  {
 	private static String username = "fpuser";
 	private static String password = "510";
 
-	public Connection connect() throws SQLException {
-
-
-		 return DriverManager.getConnection(url, username, password);
-
-
+	public DBConnect() {
+		try {
+			connection = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			System.out.println("Error creating connection to database: " + e);
+			System.exit(-1);
 		}
-		
+	}
 
-		public PreparedStatement prepareStatement(String sql) throws SQLException {
-			// TODO Auto-generated method stub
-			return connect().prepareStatement(sql);
-		}
+	
 }
-
-
-
-
-
