c0b9b78696ae8988cabfb5b2d0b6ee67efb465ce
tsharma5 <42648026+tsharma5@users.noreply.github.com>
2018-12-01 15:38:30 -0600

    reservation class edited

--- final/src/controllers/ReservationController.java
+++ final/src/controllers/ReservationController.java
@@ -1,5 +1,22 @@
 package controllers;
 
-public class ReservationController {
+import java.time.LocalDateTime;
 
+public class ReservationController {
+	private int id;
+	private int resturantid;
+	private int customerid;
+	private int tableid;
+	private LocalDateTime dateTime;
+	private int numOfGuests;
+	
+	public Reservation(int id, int resturantid, int customerid, int tableid, LocalDateTime dateTime, int numOfGuests) {
+		super();
+		this.id = id;
+		this.resturantid = resturantid;
+		this.customerid = customerid;
+		this.tableid = tableid;
+		this.dateTime = dateTime;
+		this.numOfGuests = numOfGuests;
+	}	
 }
